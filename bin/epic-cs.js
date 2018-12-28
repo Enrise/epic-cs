78cbeabf1040bac9205a400e04b093b266c46cab
Rick van der Staaij <info@rickvanderstaaij.nl>
2018-12-28 10:23:52 +0100

    Include a makefile and include prettier in the eslint setup

--- bin/epic-cs.js
+++ bin/epic-cs.js
@@ -1,7 +1,6 @@
 #!/usr/bin/env node
 
-
-const packageJson = require('../package.json');
-const setup = require('../lib/setup');
+const packageJson = require("../package.json");
+const setup = require("../lib/setup");
 
 setup(packageJson.version);
